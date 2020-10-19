592fd7069231cb3a2203602b69c589d07fe0d7fa
wutango <nickkdb@gmail.com>
2020-10-18 20:06:52 -0400

    fixed bug with api callback

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var apiKey= "d8edbfd8347bc54ee0c89492b16ea074"
 var searchCity= $("#searchTerm");
-var queryURL1= "api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=" + apiKey;
 
 $(document).ready(function(){
-    $(".btn").on("click", function() {
+    $(".btn").on("click", function(e) {
+        e.preventDefault();
         searchCity = searchCity.val();
+        var queryURL1= "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey + "&units=imperial";
     
     
         $.ajax({
