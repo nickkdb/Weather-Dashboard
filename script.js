b572f34aebc193792e65627df3b54665ad45008f
wutango <nickkdb@gmail.com>
2020-10-18 22:28:29 -0400

    current weather fully functional

--- script.js
+++ script.js
@@ -2,22 +2,37 @@ var apiKey= "d8edbfd8347bc54ee0c89492b16ea074"
 var searchCity= $("#searchTerm");
 var cityTitle= $("#cityTitle");
 var temp= $("#temp");
+var humid= $("#humidity");
+var windspeed= $("#windspeed");
+var uvIndex= $("#uvindex");
 
 $(document).ready(function(){
     $(".btn").on("click", function(e) {
         e.preventDefault();
         searchCity = searchCity.val();
-        var queryURL1= "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey + "&units=imperial";
-    
+        var queryURL1= "https://api.openweathermap.org/data/2.5/weather?q=" +  searchCity + "&appid=" + apiKey + "&units=imperial";
     
         $.ajax({
             url: queryURL1,
             method: "GET"
-        }).done(function (data){
-            var result = data;
-            console.log(result);
-            setCard(result);
+        }).then(function(response){
+            var lat= response.coord.lat;
+            var lon= response.coord.lon;
+            var queryUV= "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+            $.ajax({
+                url: queryUV,
+                method: "GET"
+            }).then(function(data){
+                console.log(response);
+                console.log(data);
+                setCard(response);
+                setUV(data);
+            })
         })
+
+
+
+
     })
 
     function setCard(input) {
@@ -28,7 +43,14 @@ $(document).ready(function(){
             src: iconURL,
             alt: "icon"
         }).appendTo(cityTitle);
+        $(temp).text("Temperature: " + input.main.temp + " °F");
+        $(humid).text("Humidity: " + input.main.humidity + "%");
+        $(windspeed).text("Wind Speed: " + input.wind.speed + " MPH");
+    }
 
+    function setUV(input) {
+        var uv= input.value;
+        $(uvIndex).html("UV Index: <span> " + uv + "</span>" );
     }
 
 })
